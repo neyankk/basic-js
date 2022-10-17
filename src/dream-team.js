a62c74d39adf147cfd16e4a7be101e2bd54b0fb6
Miroslav <miroslav.shevyrev@bk.ru>
2022-10-17 20:38:18 +0300

    fix:dream-team decision

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(members) {
-  input = members
-  const makeAbbr = string => string.split(' ').map(word => word[0]).join('');
-
-  console.log(makeAbbr(members)); 
+function createDreamTeam(/* members */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
